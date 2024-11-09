9f6c1617691b955b07e60e9466ed3f48442b405c
kkmdevel <kkm.devel@gmail.com>
2024-11-09 15:39:33 +0900

    test(ParserTest): 주문 파싱 테스트 추가
    - 주문 문자열을 파싱하고 Map으로 변환되는지 검증하는 테스트 추가
    - 각 제품의 이름과 수량이 예상 값과 일치하는지 확인하는 로직 구현

--- src/test/java/store/utils/ParserTest.java
+++ src/test/java/store/utils/ParserTest.java
@@ -1,5 +1,7 @@
 package store.utils;
 
+
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -20,6 +22,29 @@ public class ParserTest {
 
     @ParameterizedTest
     @CsvSource({
+            "2024-11-07, 2024-11-07"
+    })
+    @DisplayName("올바른 날짜 변환 테스트")
+    void testParseStringToLocalDate_ValidInput(String input, String expectedDate) {
+        LocalDate expected = LocalDate.parse(expectedDate);
+        assertEquals(expected, Parser.parseStringToLocalDate(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'[item1-5],[item2-10]', 'item1', 5, 'item2', 10",
+            "'[itemA-3],[itemB-7],[itemC-1]', 'itemA', 3, 'itemB', 7, 'itemC', 1"
+    })
+    @DisplayName("올바른 주문 아이템 파싱 테스트")
+    void testParseOrderItem_ValidInput(String input, String expectedKey1, int expectedValue1, String expectedKey2, int expectedValue2) {
+        Map<String, Integer> orderItems = Parser.parseOrderItem(input);
+
+        assertEquals(expectedValue1, orderItems.get(expectedKey1));
+        assertEquals(expectedValue2, orderItems.get(expectedKey2));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
             "abc",
             "''",
             "' '"
@@ -27,16 +52,6 @@ public class ParserTest {
     @DisplayName("잘못된 정수 변환 예외 테스트")
     void testParseStringToInt_InvalidInput(String input) {
         assertThrows(IllegalArgumentException.class, () -> Parser.parseStringToInt(input));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "2024-11-07, 2024-11-07"
-    })
-    @DisplayName("올바른 날짜 변환 테스트")
-    void testParseStringToLocalDate_ValidInput(String input, String expectedDate) {
-        LocalDate expected = LocalDate.parse(expectedDate);
-        assertEquals(expected, Parser.parseStringToLocalDate(input));
     }
 
     @ParameterizedTest
@@ -50,4 +65,5 @@ public class ParserTest {
     void testParseStringToLocalDate_InvalidInput(String input) {
         assertThrows(IllegalArgumentException.class, () -> Parser.parseStringToLocalDate(input));
     }
+
 }
