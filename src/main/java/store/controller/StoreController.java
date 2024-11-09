3adc83e1f4965e6d6c0ea9b32afa5a9fd9bfc6a2
kkmdevel <kkm.devel@gmail.com>
2024-11-09 20:29:03 +0900

    feat(StoreController): 프로모션 증정 및 수량 조정 기능 구현
    - `checkAndReceivePromotionItems`: 사용자가 증정 받을 수 있는 상품에 대해 1개 추가 여부를 묻고, Y일 경우 해당 아이템의 수량을 1 증가시킴
    - `updateOrderItemsIndividually`: 재고 부족 시 프로모션 할인이 적용되지 않는 상품에 대해 수량을 조정할지 여부를 묻고, N일 경우 해당 수량만큼 조정

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,6 +1,8 @@
 package store.controller;
 
+import java.util.List;
 import java.util.Map;
+import store.domain.OrderItem;
 import store.domain.OrderItemManager;
 import store.domain.ProductManager;
 import store.domain.PromotionManager;
@@ -25,6 +27,7 @@ public class StoreController {
         displayProducts();
         OrderItemManager orderItemManager = getOrderItemsFromUser();
         PromotionResult promotionResult = applyPromotions(orderItemManager);
+        updateOrderItemsIndividually(orderItemManager, promotionResult);
     }
 
     private void displayProducts() {
@@ -39,10 +42,29 @@ public class StoreController {
     }
 
     private PromotionResult applyPromotions(OrderItemManager orderItemManager) {
+        checkAndReceivePromotionItems(orderItemManager);
         Map<String, Integer> bonuses = promotionDiscountService.calculateBonuses(orderItemManager);
         Map<String, Integer> regularPrices = promotionDiscountService.calculateRegularPrices(orderItemManager, bonuses);
         return PromotionResult.of(bonuses, regularPrices);
     }
 
+    private void checkAndReceivePromotionItems(OrderItemManager orderItemManager) {
+        List<OrderItem> canReceivePromotion = promotionDiscountService.findCanReceiveBonus(orderItemManager);
+        for (OrderItem item : canReceivePromotion) {
+            OutputView.askForReceivePromotion(item.getName());
+            if (Parser.parseYesNo(InputView.readLine())) {
+                orderItemManager.updateOrderItemPlusPromotion(item.getName());
+            }
+        }
+    }
+
+    private void updateOrderItemsIndividually(OrderItemManager orderItemManager, PromotionResult promotionResult) {
+        promotionResult.getRegularPriceMap().forEach((productName, quantity) -> {
+            OutputView.askForAdjustment(productName, quantity);
+            if (!Parser.parseYesNo(InputView.readLine())) {
+                orderItemManager.updateOrderItemAdjustment(productName, quantity);
+            }
+        });
+    }
 
 }
