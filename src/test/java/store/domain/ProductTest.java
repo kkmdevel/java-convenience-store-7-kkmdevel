f5f401db9d36cd9be06d6f1b76c786a0628a809d
kkmdevel <kkm.devel@gmail.com>
2024-11-09 16:09:11 +0900

    test(ProductTest): Product 객체 생성 테스트 수정
    - Product의 이름, 가격, 수량, 프로모션 값이 올바르게 설정되는지 검증메소드 이름 변경

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -9,14 +9,14 @@ public class ProductTest {
     @ParameterizedTest
     @CsvSource({
             "'콜라', 1000, 10, '탄산2+1'",
-            "'사이다', 1200, 5, ''"
+            "'사이다', 1200, 5, 'null'"
     })
     void testProductCreation(String name, int price, int quantity, String promotion) {
-        if (promotion.isEmpty()) promotion = null;
         Product product = Product.of(name, price, quantity, promotion);
-        assertEquals(name, product.getName());
-        assertEquals(price, product.getPrice());
-        assertEquals(quantity, product.getQuantity());
+
+        assertTrue(product.hasName(name));
+        assertEquals(price, product.calculatePrice(1));
+        assertEquals(quantity, product.availableQuantity());
         assertEquals(promotion, product.getPromotion());
     }
 }
