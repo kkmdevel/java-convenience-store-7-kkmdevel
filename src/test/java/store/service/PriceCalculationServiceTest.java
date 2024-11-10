97fca8b1c602a7575351628b7c2f738bb7a3f547
kkmdevel <kkm.devel@gmail.com>
2024-11-10 10:21:56 +0900

    test(PriceCalculationServiceTest): 총 가격 계산 테스트
    - 아이템 수량과 가격을 기반으로 총 가격이 올바르게 계산되는지 테스트

---
+++ src/test/java/store/service/PriceCalculationServiceTest.java
@@ -0,0 +1,37 @@
+package store.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PriceCalculationServiceTest {
+
+    private PriceCalculationService priceCalculationService;
+    private OrderItemManager orderItemManager;
+
+    @BeforeEach
+    void setUp() {
+        ProductManager productManager = ProductManager.load();
+        priceCalculationService = new PriceCalculationService(productManager);
+
+        orderItemManager = OrderItemManager.from(Map.of(
+                "콜라", 5,
+                "사이다", 3
+        ));
+    }
+
+    @Test
+    @DisplayName("총 가격 계산 테스트")
+    void calculateTotalPriceTest() {
+        List<Integer> totalPrices = priceCalculationService.calculateTotalPrice(orderItemManager);
+
+        assertThat(totalPrices.get(0)).isEqualTo(1000 * 5);
+        assertThat(totalPrices.get(1)).isEqualTo(1000 * 3);
+    }
+}
