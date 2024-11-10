4d1d644482c50d2b152eb5fc6a81197eaf0fd4e4
kkmdevel <kkm.devel@gmail.com>
2024-11-10 16:23:50 +0900

    feat(PromotionController): 프로모션 관련 로직 분리
    - 프로모션 적용 및 보너스 출력 로직을 PromotionController로 이동
    - 프로모션 혜택을 적용하는 로직을 분리하여 책임을 분산

---
+++ src/main/java/store/controller/PromotionController.java
@@ -0,0 +1,55 @@
+package store.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import store.domain.OrderItem;
+import store.domain.OrderItemManager;
+import store.domain.PromotionResult;
+import store.service.PromotionDiscountService;
+import store.utils.Parser;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class PromotionController {
+    private final PromotionDiscountService promotionDiscountService;
+
+    public PromotionController(PromotionDiscountService promotionDiscountService) {
+        this.promotionDiscountService = promotionDiscountService;
+    }
+
+    public PromotionResult applyPromotions(OrderItemManager orderItemManager) {
+        checkAndReceivePromotionItems(orderItemManager);
+        Map<String, Integer> bonuses = promotionDiscountService.calculateBonuses(orderItemManager);
+        Map<String, Integer> regularPrices = promotionDiscountService.calculateRegularPrices(orderItemManager, bonuses);
+        return PromotionResult.of(bonuses, regularPrices);
+    }
+
+    private void checkAndReceivePromotionItems(OrderItemManager orderItemManager) {
+        List<OrderItem> canReceivePromotion = promotionDiscountService.findCanReceiveBonus(orderItemManager);
+        for (OrderItem item : canReceivePromotion) {
+            OutputView.askForReceivePromotion(item.getName());
+            if (Parser.parseYesNo(InputView.readLine())) {
+                orderItemManager.updateOrderItemPlusPromotion(item.getName());
+            }
+        }
+    }
+
+    public void updateOrderItemsIndividually(OrderItemManager orderItemManager, PromotionResult promotionResult) {
+        promotionResult.getRegularPriceMap().forEach((productName, quantity) -> {
+            if (quantity > 0) {
+                OutputView.askForAdjustment(productName, quantity);
+                if (!Parser.parseYesNo(InputView.readLine())) {
+                    orderItemManager.updateOrderItemAdjustment(productName, quantity);
+                }
+            }
+        });
+    }
+
+    public void displayBonus(PromotionResult promotionResult) {
+        Map<String, Integer> filteredBonusMap = promotionResult.getBonusMap().entrySet().stream()
+                .filter(entry -> entry.getValue() > 0)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        OutputView.printBonusItems(filteredBonusMap);
+    }
+}
