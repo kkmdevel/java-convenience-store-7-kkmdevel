eed80055273f27eeed6f5d0341e75ccdf2e9dc75
kkmdevel <kkm.devel@gmail.com>
2024-11-10 15:11:35 +0900

    test(ProductManagerTest): 상품 가격 계산, 프로모션 정보 조회, 재고 감소 기능 테스트 추가
    - 특정 상품의 프로모션 정보를 반환하는 기능에 대한 테스트 추가
    - 프로모션 재고와 일반 재고를 정확히 반환하는 기능에 대한 테스트 추가
    - 프로모션 재고와 일반 재고를 차감하는 기능에 대한 테스트 추가

--- src/test/java/store/domain/ProductManagerTest.java
+++ src/test/java/store/domain/ProductManagerTest.java
@@ -9,11 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ProductManagerTest {
 
+    private static ProductManager productManager;
     private static List<Product> products;
 
     @BeforeAll
     static void setUp() {
-        ProductManager productManager = ProductManager.load();
+        productManager = ProductManager.load();
         products = productManager.getProducts();
     }
 
@@ -27,5 +28,42 @@ public class ProductManagerTest {
                 () -> assertThat(products.getFirst().availableQuantity()).isEqualTo(10),
                 () -> assertThat(products.getFirst().getPromotion()).isEqualTo("탄산2+1")
         );
+    }
+
+    @Test
+    @DisplayName("요청한 수량에 맞는 상품 가격을 정확히 계산한다.")
+    void calculatePrice() {
+        int price = productManager.calculatePrice("콜라", 5);
+        assertThat(price).isEqualTo(1000 * 5);
+    }
+
+    @Test
+    @DisplayName("특정 상품의 프로모션 정보를 반환한다.")
+    void findPromotion() {
+        String promotion = productManager.findPromotion("콜라");
+        assertThat(promotion).isEqualTo("탄산2+1");
+    }
+
+    @Test
+    @DisplayName("프로모션 재고와 일반 재고를 정확히 반환한다.")
+    void getStockInformation() {
+        int promotionStock = productManager.getPromotionStock("콜라");
+        int regularStock = productManager.getRegularStock("콜라");
+
+        assertThat(promotionStock).isEqualTo(10);
+        assertThat(regularStock).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("ProductManager는 프로모션 재고와 일반 재고를 정확히 차감한다.")
+    void reduceStock() {
+        int initialPromotionStock = productManager.getPromotionStock("콜라");
+        int initialRegularStock = productManager.getRegularStock("콜라");
+
+        productManager.reducePromotionStock("콜라", 3);
+        productManager.reduceRegularStock("콜라", 2);
+
+        assertThat(productManager.getPromotionStock("콜라")).isEqualTo(initialPromotionStock - 3);
+        assertThat(productManager.getRegularStock("콜라")).isEqualTo(initialRegularStock - 2);
     }
 }
