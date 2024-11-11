d9fec5f64887983cc2ab4ff7f170e2c84661c913
kkmdevel <kkm.devel@gmail.com>
2024-11-11 12:18:29 +0900

    refactor(OrderItemManager): 에러 메세지 수정

--- src/main/java/store/domain/OrderItemManager.java
+++ src/main/java/store/domain/OrderItemManager.java
@@ -40,7 +40,7 @@ public class OrderItemManager {
             int availableStock = productManager.calculateTotalStockForProduct(productName);
 
             if (requestedQuantity > availableStock) {
-                throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException();
             }
         }
     }
