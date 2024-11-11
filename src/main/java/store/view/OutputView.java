da3ffa397f0d0fbcb3bd5c14ee2337aa14973bce
kkmdevel <kkm.devel@gmail.com>
2024-11-11 12:11:42 +0900

    feat(OutputView): 출력 메시지 간 공백 추가
    - 주요 출력 섹션 앞뒤에 공백 라인을 추가하여 가독성 향상
    - printBlankLine() 메서드를 사용해 출력 구분을 명확하게 처리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -14,16 +14,22 @@ public class OutputView {
         return NumberFormat.getInstance().format(price);
     }
 
+    public static void printBlankLine() {
+        System.out.println();
+    }
+
     public static void printWelcomeAndAllProducts(ProductManager productManager) {
+        printBlankLine();
         System.out.println(WELCOME.getMessage());
         System.out.println(ALL_PRODUCTS_PREFIX.getMessage());
-        System.out.println();
+        printBlankLine();
 
         List<Product> products = productManager.getProducts();
 
         for (Product product : products) {
             System.out.println(formatProductMessage(product));
         }
+        printBlankLine();
     }
 
     private static String formatProductMessage(Product product) {
@@ -38,10 +44,10 @@ public class OutputView {
     }
 
     private static String formatQuantityMessage(Product product) {
-        if(product.availableQuantity()==0){
+        if (product.availableQuantity() == 0) {
             return "재고 없음";
         }
-        return product.availableQuantity() +"개";
+        return product.availableQuantity() + "개";
     }
 
     private static String formatPromotionMessage(Product product) {
@@ -51,67 +57,85 @@ public class OutputView {
         return product.getPromotion();
     }
 
-    public static void printOrderPrefix(){
-        System.out.println();
+    public static void printOrderPrefix() {
+        printBlankLine();
         System.out.println(PURCHASE_PREFIX.getMessage());
+        printBlankLine();
     }
 
     public static void askForReceivePromotion(String name) {
-        System.out.println();
+        printBlankLine();
         System.out.println(CAN_RECEIVE_BONUS.getFormattedMessage(name));
+        printBlankLine();
     }
 
     public static void askForAdjustment(String productName, Integer quantity) {
-        System.out.println();
-        System.out.println(ADJUSTMENT_QUANTITY.getFormattedMessage(productName,quantity));
+        printBlankLine();
+        System.out.println(ADJUSTMENT_QUANTITY.getFormattedMessage(productName, quantity));
+        printBlankLine();
     }
 
     public static void askForMembershipDiscount() {
-        System.out.println();
+        printBlankLine();
         System.out.println(RECEIVE_MEMBERSHIP_DISCOUNT.getMessage());
+        printBlankLine();
     }
 
     public static void printOriginalPrice(Map<String, Integer> purchasedItems, List<Integer> totalPrices) {
-        System.out.println();
+        printBlankLine();
         System.out.println(START_RECEIPT.getMessage());
         System.out.println(ORIGINAL_PRICE_PREFIX.getMessage());
+        printBlankLine();
         int index = 0;
         for (String name : purchasedItems.keySet()) {
             int quantity = purchasedItems.get(name);
             int totalPrice = totalPrices.get(index++);
             System.out.println(PURCHASED_ITEMS.getFormattedMessage(name, quantity, formatPrice(totalPrice)));
         }
+        printBlankLine();
     }
 
     public static void printBonusItems(Map<String, Integer> bonusMap) {
+        printBlankLine();
         System.out.println(BONUS_ITEMS_PREFIX.getMessage());
-        bonusMap.forEach((name, bonusQuantity) -> System.out.println(BONUS_ITEMS.getFormattedMessage(name,bonusQuantity)));
+        bonusMap.forEach((name, bonusQuantity) -> System.out.println(BONUS_ITEMS.getFormattedMessage(name, bonusQuantity)));
+        printBlankLine();
     }
 
     public static void printTotalPrice(int totalItemQuantity, int totalPriceSum) {
+        printBlankLine();
         System.out.println(CALCULATION_DISCOUNT_PREFIX.getMessage());
-        System.out.println(TOTAL_PRICE.getFormattedMessage(totalItemQuantity,formatPrice(totalPriceSum)));
+        System.out.println(TOTAL_PRICE.getFormattedMessage(totalItemQuantity, formatPrice(totalPriceSum)));
+        printBlankLine();
     }
 
     public static void printPromotionDiscountPrices(int totalPromotionDiscountAmount) {
+        printBlankLine();
         System.out.println(PROMOTION_DISCOUNT.getFormattedMessage(formatPrice(totalPromotionDiscountAmount)));
+        printBlankLine();
     }
 
     public static void printMembershipDiscount(int membershipDiscountAmount) {
+        printBlankLine();
         System.out.println(MEMBERSHIP_DISCOUNT.getFormattedMessage(formatPrice(membershipDiscountAmount)));
+        printBlankLine();
     }
 
     public static void printPayableAmount(int payableAmount) {
+        printBlankLine();
         System.out.println(PAYABLE_AMOUNT.getFormattedMessage(formatPrice(payableAmount)));
+        printBlankLine();
     }
 
     public static void askToContinueShopping() {
-        System.out.println();
+        printBlankLine();
         System.out.println(CONTINUE_SHOPPING.getMessage());
+        printBlankLine();
     }
 
-    public static void printErrorMessage(String ErrorMessage){
-        System.out.println();
-        System.out.println(ErrorMessage);
+    public static void printErrorMessage(String errorMessage) {
+        printBlankLine();
+        System.out.println(errorMessage);
+        printBlankLine();
     }
 }
